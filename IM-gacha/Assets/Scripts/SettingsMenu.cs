1d0cfad4b8d4bc8cf45be8e330efc96fc7d4a3e3
tibaa003 <91213295+tibaa003@users.noreply.github.com>
2021-11-10 12:01:24 +0100

    fixed settings bugs
    Still a few bugs with resolution to fullscreen shit fuck thing

--- IM-gacha/Assets/Scripts/SettingsMenu.cs
+++ IM-gacha/Assets/Scripts/SettingsMenu.cs
@@ -24,12 +24,14 @@ public class SettingsMenu : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " (" + resolutions[i].refreshRate + "Hz)";
             options.Add(option);
+
+            Debug.Log(resolutions[i]);
 
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
-                currentResolutionIndex = 1;
+                currentResolutionIndex = i;
             }
         }
 
