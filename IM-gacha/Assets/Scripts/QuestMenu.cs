98e747cd152b50c275adb22c1c88eb8ed1c4e862
Srimmy <srpra001@osloskolen.no>
2021-11-12 11:16:01 +0100

    la til script for story og quests
    sadsad

--- IM-gacha/Assets/Scripts/QuestMenu.cs
+++ IM-gacha/Assets/Scripts/QuestMenu.cs
@@ -5,6 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class QuestMenu : MonoBehaviour
 {
+    public GameObject Story;
+    public GameObject Quests1;
+    public GameObject Quests2;
+    public GameObject Quests3;
+
+
+    public void ShowQuests(int Chapter)
+    {
+
+        Quests1.SetActive(false);
+        Quests2.SetActive(false);
+        Quests3.SetActive(false);
+        Story.SetActive(false);
+
+
+        if (Chapter == 1)
+        {
+            if (Quests1.activeInHierarchy == true)
+            {
+                Quests1.SetActive(false);
+            }
+            else
+            {
+                Quests1.SetActive(true);
+                
+            }
+        }
+
+        if (Chapter == 2)
+        {
+            if (Quests2.activeInHierarchy == true)
+            {
+                Quests2.SetActive(false);
+            }
+            else
+            {
+                Quests2.SetActive(true);
+            }
+        }
+
+        if (Chapter == 3)
+        {
+            if (Quests3.activeInHierarchy == true)
+            {
+                Quests3.SetActive(false);
+            }
+            else
+            {
+                Quests3.SetActive(true);
+            }
+        }
+
+
+    }
     public void LoadScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
@@ -13,4 +67,27 @@ public class QuestMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    public void ShowStory()
+    {
+        if (Story.activeInHierarchy == true)
+        {
+
+
+            Story.SetActive(false);
+
+        }
+
+        else
+        {
+            Story.SetActive(true);
+            Quests1.SetActive(false);
+            Quests2.SetActive(false);
+            Quests3.SetActive(false);
+        }
+
+    }
 }
+
+
+ 
+
