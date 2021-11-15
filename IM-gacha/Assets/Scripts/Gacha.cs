e9e60bbd73ec1fa7429a476f19f973efebe6042d
tibaa003 <91213295+tibaa003@users.noreply.github.com>
2021-11-15 11:02:36 +0100

    Made foundation/grunnmur
    Created summon scene and scripts associated

---
+++ IM-gacha/Assets/Scripts/Gacha.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gacha : MonoBehaviour
+{
+    public static bool multi;
+
+    void Start(){
+        if(!multi){
+            //srimon gacha moment
+        } else{
+            for(int i = 0; i < 9; i++){
+                //srimon moment
+            }
+        }
+    }
+}
--- IM-gacha/Assets/Scripts/SceneLoader.cs
+++ IM-gacha/Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string SceneName)
+    public static void LoadScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
     }
