5cb20031370eb89f4ba4ffd830e50a746a50b974
Jokra005 <jokra005@osloskolen.no>
2021-11-19 10:00:12 +0100

    Rewrite some scripts
    Signed-off-by: Jokra005 <jokra005@osloskolen.no>

--- IM-gacha/Assets/Scripts/GachaSystem/Gacha.cs
+++ IM-gacha/Assets/Scripts/GachaSystem/Gacha.cs
@@ -1,123 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
 
 public class Gacha : MonoBehaviour
 {
-    public static bool multi;
+	public static bool multi;
 
-    public int[] table =
-     {
-        30, //4 star
-        10, //5 star
-        60
-    };
-    public int total;
-    public int randomNumber;
-    public int rn;
-    private int currentPull = 0;
-    public Sprite drop;
-    static int random;
-    public GameObject singleIcon;
+	public int[] table =
+	{
+		30, //4 star
+		10, //5 star
+		60
+	};
+	public int total;
+	public int randomNumber;
+	private int currentPull = 0;
+	public Sprite drop;
+	static int random;
+	public GameObject singleIcon;
 
-    public GameObject[] multiIcons;
+	public GameObject[] multiIcons;
 
-    public List<Sprite> star3 = new List<Sprite>();
-    public List<Sprite> star4 = new List<Sprite>();
-    public List<Sprite> star5 = new List<Sprite>();
-    void Start()
-    {
-        //Animatio
-        if (!multi)
-        {
-            total = 0;
-            foreach (var item in table)
-            {
-                total += item; //finner ut hvor mange total weight er
-            }
+	public List<Sprite> star3 = new List<Sprite>();
+	public List<Sprite> star4 = new List<Sprite>();
+	public List<Sprite> star5 = new List<Sprite>();
+	void Start()
+	{
+		int count = multi ? 10 : 1;
+		total = table.Sum();
+		for (int i = 0; i < count; i++)
+		{
+			randomNumber = UnityEngine.Random.Range(0, total);
 
-            randomNumber = UnityEngine.Random.Range(0, total);
-            rn = randomNumber;
+			for (int j = 0; j < table.Length; j++)
+			{
+				if (randomNumber <= table[j])
+				{
+					character(table[j]);
+					break;
+				}
+				else
+				{
+					randomNumber -= table[j];
+				}
+			}
+		}
+	}
+	public void character(int rating)
+	{
+		switch (rating)
+		{
+			case 10:
+				random = UnityEngine.Random.Range(0, star5.Count);
+				drop = star5[random];
+				Debug.Log(drop);
+				break;
+			case 30:
+				random = UnityEngine.Random.Range(0, star4.Count);
+				drop = star4[random];
+				Debug.Log(drop);
+				break;
+			case 60:
+				random = UnityEngine.Random.Range(0, star3.Count);
+				drop = star3[random];
+				Debug.Log(drop);
+				break;
+		}
+		createBox(drop);
+	}
+	public void createBox(Sprite e)
+	{
+		if (!multi)
+		{
+			singleIcon.SetActive(true);
+			singleIcon.GetComponent<Image>().sprite = e;
+		}
+		else
+		{
+			multiIcons[currentPull].SetActive(true);
+			multiIcons[currentPull].GetComponent<Image>().sprite = e;
+			currentPull++;
+		}
 
-            for (int i = 0; i < table.Length; i++)
-            {
-                if (randomNumber <= table[i])
-                {
-                    character(table[i]);
-                    break;
-                }
-                else
-                {
-                    randomNumber -= table[i];
-                }
-
-            }
-        }
-        else
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                total = 0;
-                foreach (var item in table)
-                {
-                    total += item; //finner ut hvor mange total weight er
-                }
-
-                randomNumber = UnityEngine.Random.Range(0, total);
-                rn = randomNumber;
-
-                for (int j = 0; j < table.Length; j++)
-                {
-                    if (randomNumber <= table[j])
-                    {
-                        character(table[j]);
-                        break;
-                    }
-                    else
-                    {
-                        randomNumber -= table[j];
-                    }
-                }
-            }
-
-        }
-    }
-    public void character(int rating)
-    {
-        if (rating == 10)
-        {
-            random = UnityEngine.Random.Range(0, star5.Count);
-            drop = star5[random];
-            Debug.Log(drop);
-        }
-        else if (rating == 30)
-        {
-            random = UnityEngine.Random.Range(0, star4.Count);
-            drop = star4[random];
-            Debug.Log(drop);
-        } else if (rating == 60){
-            random = UnityEngine.Random.Range(0, star3.Count);
-            drop = star3[random];
-            Debug.Log(drop);
-        }
-        createBox(drop);
-    }
-    public void createBox(Sprite e)
-    {
-        if (!multi)
-        {
-            singleIcon.SetActive(true);
-            singleIcon.GetComponent<Image>().sprite = e;
-        }
-        else
-        {
-            multiIcons[currentPull].SetActive(true);
-            multiIcons[currentPull].GetComponent<Image>().sprite = e;
-            currentPull++;
-        }
-
-    }
+	}
 }
-
